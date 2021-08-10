6aad2f9e716468c2b975ce6e67d28b4743013ad6
MaksimUrasov <maksim.urasov@gmail.com>
2021-08-10 09:10:27 +0300

    last version before MVC

--- button_pressed_logic.php
+++ button_pressed_logic.php
@@ -7,151 +7,95 @@ session_destroy(); // This is necessary to delete old data, from previous "send 
 session_start();   //destroying the Session from starting a new session, so I have to start session again
 
 
-// connect to DB
-require_once 'manage_db.php';
-$table_name = Connect_to_db_singletone_modified::$table_name;
-$confirmation_to_client_message_saved = "Your message has been saved. Thank you!"; 
-
-function test_input($data) {
-    $data = trim($data); //Strip unnecessary characters (extra space, tab, newline)
-    $data = stripslashes($data); //Remove backslashes (\) 
-    $data = htmlspecialchars($data); //converts special characters to HTML entities.
-    return $data;
-};
-$first_name = $last_name = $birth = $email = $message = $name = "";// to have global variables scope
-
-//below we check where the data came from- from index.php by POST method or by AJAX POST method:
-
-if (array_key_exists("first_name", $_POST)) { // true means info came from index.php 
-    // var_dump($_POST);
-    if(validate_and_save_errors_to_session($_POST["first_name"],$_POST["last_name"],$_POST["birth"],$_POST["email"],$_POST["message"])){
-        //Function validate_and_save_errors_to_session saves error messages to SESSION and returns true if there are errors. 
-        // if there are some errors, have to save each POST value to $_SESSION to be shown again on index.php page and return to index.php 
-                    
-        global $first_name, $last_name, $birth, $email, $message;
-        $_SESSION['first_name']= $first_name; 
-        $_SESSION['last_name']= $last_name;  
-        $_SESSION['birth']= $birth;
-        $_SESSION['email']= $email;
-        $_SESSION['message']= $message;
-
-
-    } //if there are no errors, wll run send_info_to_DB
-        
-    $name = $first_name." ".$last_name;
-    $email = $email ?: "NULL"; // to save text "NULL" to DB on later stage
-    $sending_to_DB_result = send_info_to_DB($name,$birth,$email,$message); // this function not only saves to DB, but as confirmation also returns an array.
-
-    if($sending_to_DB_result[0]){
-        //as long as we redirect, have to save messages to Session:
-        $_SESSION['DB_updated'] = $confirmation_to_client_message_saved;
-    }else{
-        $_SESSION['DB_error'] = "Something went wrong, message not saved to DB. <br>" . "Request: ". $sending_to_DB_result[1] . "<br>" . $sending_to_DB_result[2];
-    };
-
-    // echo "there are some errors, now page will be redirected to index";
-    header("Location: index.php");
-    exit();
-        
-    
-
-
-}else {
-    $data = json_decode(file_get_contents("php://input"));
-    // echo "Server has has received POST request from AJAX. Received data: " . $data . "<br>";
-    // var_dump($data);
-    // var_dump($_POST);
- 
-    if(validate_and_save_errors_to_session($data->first_name,$data->last_name,$data->birth,$data->email,$data->message)){
-        //Function validate_and_save_errors_to_session returns true if there are errors. 
-        // if there are some errors, have to save each POST value to $_SESSION to be shown again on index.php page and return to index.php 
-        echo "That is very weird, but seems you have passed the wrong data in input fields. PHP input validator has found some mistakes.
-        Try to refresh a page and resubmit a form. <br> Values and Errors: ";
-        var_dump($_SESSION);
-    };
-
-    $name = $first_name." ".$last_name; 
-    $email = $email ?: "NULL"; // to save text "NULL" to DB on later stage
-    $sending_to_DB_result = send_info_to_DB($name,$birth,$email,$message);
-
-    if($sending_to_DB_result[0]){
-        echo $sending_to_DB_result[0];
-
-    }else{
-        echo "Something went wrong, message not saved to DB. <br>" . "Request: ". $sending_to_DB_result[1] . "<br>" . $sending_to_DB_result[2];
-    };
-
-    // there is no redirect back to index.php
-
-
-} 
-    
-//this piece of code shall be executed any way:
-
-
-
-
-
 // $first_name = $last_name = $birth = $email = $message = "";
 // $first_name_err = $last_name_err = $birth_err = $email_err = $message_err = "";
 
-
-function validate_and_save_errors_to_session($fn, $ln, $b, $e, $m){ // JS makes same validation in browser, but it is better to recheck data on server      
-    global $name, $first_name, $last_name, $birth, $email, $message; // I will need them in global scope so save in DB on send_info_to_DB()
-
-    $first_name = test_input($fn); 
-    $last_name = test_input($ln);  
-    $birth = test_input($b); // we save birth date to DB, exact age of customer will be calculated on loading the message.
-    $email = test_input($e);
-    $message = test_input($m);
-    $there_is_an_error= false;
-
-    if(!preg_match("/^[a-zA-Z-' ]*$/",$first_name)){
-        $_SESSION['first_name_err'] = "shall contain only letters and whitespaces.";
-        $there_is_an_error= true;
-    };
-
-    if(!preg_match("/^[a-zA-Z-' ]*$/",$last_name)){
-        $_SESSION['last_name_err'] = "shall contain only letters and whitespaces.";
-        $there_is_an_error= true;
-    };
-
-
-    if($birth > date("Y-m-d")){
-        $_SESSION['birth_err'] = " can not be in the future!"; // the beginning of the sentence " *Your date of birth" is already displayed.
-        $there_is_an_error= true;
-    };
-
-
-    if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $_SESSION['email_err'] = ": Seems there is a typing error.";  
-        $there_is_an_error= true;
-    };
-
-    if(strlen($message)<3){
-        $_SESSION['message_err']=  "shall contain at least 3 characters";
-        $there_is_an_error= true;
-    } else if(strlen($message)>500){
-        $_SESSION['message_err']=  "shall contain less than 500 characters";  // this is DB limitation I have set for this field.
-        $there_is_an_error= true;
-    };
-    
-    return $there_is_an_error;
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 };
 
-    
+      
 
 
-function send_info_to_DB($name,$birth,$email,$message){ // here we check if there are error messages savedin SESSION and return to index.php or save info to DB and return.
-    
-    
-    // $first_name = test_input($_POST["first_name"]); // each POST value has to be saved to $_SESSION to be shown again if there are errors.
-    // $last_name = test_input($_POST["last_name"]);  // if there are no errors, $these 
-    // $name = $first_name." ".$last_name;
 
-    // $birth = test_input($_POST["birth"]); 
-    // $email = test_input($_POST["email"]) ?: "NULL";
-    // $message = test_input($_POST["message"]);
+function validate_and_save_errors_to_session(){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // echo "post method received" . "<br>";
+        // print_r($_POST);
+    
+        if(!preg_match("/^[a-zA-Z-' ]*$/",$_POST["first_name"])){
+            $_SESSION['first_name_err'] = "Name shall contain only letters and whitespaces.";
+            // add_css_red("first_name");
+        };
+    
+        if(!preg_match("/^[a-zA-Z-' ]*$/",$_POST["last_name"])){
+            $_SESSION['last_name_err'] = "Last name shall contain only letters and whitespaces.";
+            // add_css_red("last_name");
+        };
+    
+    
+        if($_POST["birth"] > date("Y-m-d")){
+            $_SESSION['birth_err'] = " can not be in the future!"; // the beginning of the sentence " *Your date of birth" is already displayed.
+            // add_css_red("birth");
+        };
+    
+    
+        if(!empty($_POST["email"]) && !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+            $_SESSION['email_err'] = "Seems there is an error in the email";
+            // add_css_red("email");     
+        };
+    
+        if(strlen($_POST["message"])<3){
+            $_SESSION['message_err']=  "Your message shall contain at least 3 characters";
+            // add_css_red("message");   
+        } else if(strlen($_POST["message"])>500){
+            $_SESSION['message_err']=  "Your message shall contain less than 500 characters";  // this is DB limitation I have set for this field.
+            // add_css_red("message");
+        };
+    };
+};
+
+validate_and_save_errors_to_session();
+
+// connect to DB
+require_once 'manage_db.php';
+
+$table_name = Connect_to_db_singletone_modified::$table_name;
+
+function return_or_send_info_to_DB(){ // here we check if there are error messages savedin SESSION and return to index.php or save info to DB and return.
+    //1) option one- there are errors and we have to return
+    foreach($_SESSION as $key => $value ){
+        if(str_contains($key, "err")) {
+            // error messages are already saved to SESSION
+            // each POST value has to be saved to $_SESSION to be shown again:
+            $_SESSION['first_name']= test_input($_POST["first_name"]); 
+            $_SESSION['last_name']= test_input($_POST["last_name"]);  
+            $_SESSION['birth']= test_input($_POST["birth"]);
+            $_SESSION['email']= test_input($_POST["email"]);
+            $_SESSION['message']= test_input($_POST["message"]);
+
+
+            header("Location: index.php");
+            exit();
+            // echo "<br> $key";
+        } 
+    }
+
+    
+    
+    //2) option two- there are no errors and we have to save the info to DB and then return
+     
+    
+    $first_name = test_input($_POST["first_name"]); // each POST value has to be saved to $_SESSION to be shown again if there are errors.
+    $last_name = test_input($_POST["last_name"]);  // if there are no errors, $these 
+    $name = $first_name." ".$last_name;
+
+    $birth = test_input($_POST["birth"]); // we save birth date to DB, exact age of customer will be calculated on loading the message.
+    $email = test_input($_POST["email"]) ?: "NULL";
+    $message = test_input($_POST["message"]);
 
     // insert row into DB table
     try {
@@ -165,19 +109,22 @@ function send_info_to_DB($name,$birth,$email,$message){ // here we check if ther
         $connection_object = new Connections_to_db;
         $connection_object->db_insert($sql,$name,$birth,$email,$message);
         
-        $success = true; 
-        return array($success);
+        $_SESSION['DB_updated']="Your message has been saved. Thank you!";
+
 
     } catch(PDOException $e) {
-        $success = false; 
+        // echo "Error: " . $e->getMessage();  // this shows error on same page, but as long as we redirect, have to save error to Session:
+        $_SESSION['DB_error']="Something went wrong, message not saved to DB. <br>" . "Request: ". $sql . "<br>" . $e->getMessage();
     }
-    return array($success, $sql, $e->getMessage());
     
-    //echo "message sent to DB";
+    header("Location: index.php");
+    exit;
+
+    echo "message sent to DB";
 
 }
 
-
+return_or_send_info_to_DB();
 
 // the last thing that has to be done when all page elements are loaded - close connection to DB.
 // Connections_to_db::db_close();
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messaging board</title>
     <link rel="stylesheet" href="/styles.css">
-    
+    <script src="/script.js"></script>
         <!-- below is to delete GEThttp://localhost:3000/favicon.ico  404 ERROR -->
     <link rel="icon" type="image/ico" href="/img/1.ico" >
 </head>
@@ -20,79 +20,91 @@ require_once "functions.php";
             <h1 class="welcome">Welcome!</h1>
             <p class="welcome">Here you can leave a message for our team</p>
             <br>
-            <noscript>                     
-                <br> 
-                <p>Sorry, your browser does not support JavaScript! That means that this messaging board will not work as fast as it could.</p>
-                <br>
-            </noscript>
             
-            <form  method="post" action="/button_pressed_logic.php" onsubmit="Actions()" >  
+            <form  method="post"  action="/button_pressed_logic.php" onsubmit="JSActions()" >  
                 
-                <input type="text" class="input first_name" name="first_name" value="<?php get_session_value("first_name")?>" placeholder="First name"   required>
-                <div class="error_message first_name_err">* First Name <?php get_session_value("first_name_err")?></div><br>
+                <input type="text" name="first_name" value="<?php get_session_value("first_name")?>" placeholder="First name" class="input first_name"  required>
+                <div class="error_message first_name_err">* <?php get_session_value("first_name_err")?></div><br>
 
-                <input type="text" class="input last_name" name="last_name" value="<?php get_session_value("last_name")?>" placeholder="Last name"  required>
-                <div class="error_message last_name_err">* Last name <?php get_session_value("last_name_err")?></div><br>
+                <input type="text" name="last_name" value="<?php get_session_value("last_name")?>" placeholder="Last name" class="input last_name" required>
+                <div class="error_message last_name_err">* <?php get_session_value("last_name_err")?></div><br>
 
-                <input type="date" class="input birth" name="birth" value="<?php get_session_value("birth")?>" placeholder="Your date of birth"  required>
-                <div class="error_message birth_err">* Your date of birth <?php get_session_value("birth_err")?></div><br>
+                <input type="date" name="birth" value="<?php get_session_value("birth")?>" placeholder="Your date of birth" class="input birth" required>
+                <div class="error_message birth_err">*Your date of birth <?php get_session_value("birth_err")?></div><br>
 
-                <input type="email" class="input email" name="email" value="<?php get_session_value("email")?>" placeholder="Your email" >
-                <div class="error_message email_err">  E-mail<?php get_session_value("email_err")?></div><br>
+                <input type="email" name="email" value="<?php get_session_value("email")?>" placeholder="Your email" class="input email">
+                <div class="error_message email_err"><?php get_session_value("email_err")?></div><br>
 
-                <textarea name="message" class="input message" placeholder="Please enter your message here"  required><?php get_session_value("message")?></textarea >
-                <div class="error_message message_err">* Message <?php get_session_value("message_err")?></div><br>
+                <textarea name="message"  placeholder="Please enter your message here" class="input message" required><?php get_session_value("message")?></textarea >
+                <div class="error_message message_err">* <?php get_session_value("message_err")?></div><br>
 
                 <div class="mandatory_notice">*These fields are mandatory</div><br>
-                
-                <div id="button_created_by_JS"></div>
-
-                <button class="button" type="submit">Send your message</button>
-
-                <div class="lds-facebook"><div></div><div></div><div></div></div> <!-- copied from https://loading.io/css/ -->
+                <button type="submit">Send your message</button>
 
             </form>
 
-            <div id='server_success_message'><?php echo $server_message_ok; ?></div>
-            <div id='server_error_message'><?php echo $server_message_err; ?></div>
+            <div class='server_success_message'><?php echo $server_message_ok; ?></div>
+            <div class='server_error_message'><?php echo $server_message_err; ?></div>
             
 
             <!-- <button onclick="JSActions()" >test JS</button>  -->
             
-            <section id="container_for_old_messages">
-                <p id="name_for_message_container" >Message history</p>
+            <section class="container_for_old_messages">
+                <p class="message_container_name" id="messages" >Message history</p>
                 <?php
-                //3)
                 Loading_messages::download_old_messages();
+                Loading_messages::download_amount_of_old_messages();
                 ?>
-                <!-- <div class='container_for_one_old_message'>  // this is the example message
+                <!-- <div class='container_for_one_old_message'>
                     <div class="name_and_year_container">
                         <p class="old_name">Darius Kaimynas</p> 
                         <p class="old_age">31 years</p> 
                     </div>
                     <p class="old_message">Laba diena!</p> 
-                </div> -->
+                </div>
 
+
+            </section>  the whole section is generated in the load_messages.php -->
             </section>
-            <div class='page_links_container'>
-                <?php
-                    Create_links_to_pages::create_page_links($amount_of_pages, $page_to_show); 
-                ?>
+            <div class='pages_container'>
+            <?php
+                Create_links_to_pages::create_page_links($amount_of_entries, $number_of_posts_per_page); 
+            ?>
             </div>
-            
+
+
         </main>
     </body>
-    <script src="/script.js"></script> 
 </html>
 
-
-
 <!-- 
-// I chose JSON because it is faster and better than XML: https://www.w3schools.com/js/js_json_xml.asp
 
 
-mvc pattern
-JS OOP
+
+Siuo atveju kalbama apie OOP, reiketu susikurti klase, per kuria bendrausi su duomenu baze. Gali pasinaudoti constructorium uzmegsti 
+rysi su duomenu baze, ar sukurti metoda tam skirta.
+Taip pat gali pasidometi apie design patterns, duomenu bazei daznai naudojamas Singleton pattern, ir sis pattern'as leidzia tureti tik
+ viena originalu rysi su duomenu baze.
+Database klases tikslas butu uzmegsti rysi su duomenu baze, ir taip pat tureti klases metodus su kuriais atlieka select, insert, update, 
+delete sql uzklausas. Kad sukurus sios klases objekta, butu lengva dirbti su DB.
+
+
+If JavaScript is enabled:
+After pressing the submit button, all fields have become inactive (not editable), and instead loader diagram should appear.
+In case of success, the most recent message should be placed on top using JavaScript The oldest message should be removed from the screen. All form 
+fields should be activated again.
+In case any errors are detected, fields must be marked and fields should be activated so that user can edit their input.
+In both cases, the loader should disappear and the button should appiear instead.
+
+If JavaScript is turned off:
+Pressing the button should reload the page. 
+
+
+All messages should be paginated. Number of posts per page should be defined in constants.
+
+If an e-mail was provided, the name and last name should become a link. (By clicking on the full name, an e-mail is triggered.)
+
+Year must be calculated from the current date and the date of birth entered.
 
 
 Bonus points:
@@ -104,3 +116,8 @@ Full source code and the database structure should be sent back to us as a proof
 
 
 
+<!-- 
+Duomenų bazė:	viedis_messageboard
+Serveris (host):	localhost
+Naudotojo vardas:	viedis_root
+Slaptažodis:	barinme55ageb0ard -->
